73f2e6502fb324fd0e358447482aab9ef16ec67b
20171105121 <s20171105121@mac24.local>
2018-06-26 14:42:06 +0800

    627

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  text627
+//
+//  Created by cxm on 18/6/26.
+//  Copyright © 2018年 cxm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+
+    std::cout << "Welcome to our school\n";
+    return 0;
+}
